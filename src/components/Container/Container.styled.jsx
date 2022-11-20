5a9427673f2f7ca3a466f26fed1b5d012c044452
Вадим Фатеев <fateevdanon@gmail.com>
2022-11-20 14:00:40 +0100

    modern-normalize

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
-import constants from '../../constants';
-
-const { screens } = constants;
+// import constants from '../../constants';
+// const { screens } = constants;
 
 const ContainerStyled = styled.div`
   max-width: 1280px;
