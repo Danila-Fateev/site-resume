6c66701c403ba67d29ba7d01b93c5acc4ee562be
Вадим Фатеев <fateevdanon@gmail.com>
2022-11-22 17:38:11 +0100

    NotFoundPage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import SharedLayout from 'components/SharedLayout';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
+import NotFoundPage from 'pages/NotFoundPage';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+  return (
+    <h2
+      style={{
+        fontSize: '45px',
+        color: 'white',
+      }}
+    >
+      Not Found
+    </h2>
+  );
+};
+
+export default NotFound;
